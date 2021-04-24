96238b09fe595714ef3d022783a74f7ccd98bc66
Fun1001 <314873178@qq.com>
2021-04-24 13:58:02 +0800

    Signed-off-by: Fun1001 <314873178@qq.com>

---
+++ DXQ1/MDK-ARM/DXQ/simsun_20.d
@@ -0,0 +1,1 @@
+dxq/simsun_20.o: ..\Core\Src\SimSun_20.c ..\Drivers\OLED\GUI.H
---
+++ DXQ1/MDK-ARM/DXQ/sunhr.d
@@ -0,0 +1,2 @@
+dxq/sunhr.o: ..\Core\Src\SunHR.c \
+  E:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h ..\Drivers\OLED\GUI.h
